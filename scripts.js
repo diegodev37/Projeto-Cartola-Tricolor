17d6ecb4029b29b84b2011bf8cfe72c1e530ba3f
diegodev37 <diego.dev.37@gmail.com>
2024-04-28 15:24:20 -0300

    criei um arquivo js para os scripts.

---
+++ scripts.js
@@ -0,0 +1,14 @@
+document.getElementById("cartola-gigante3").addEventListener("click", function () {
+    document.querySelector("body").setAttribute("class", "cartola-gigant3");
+
+})
+
+document.getElementById("cartola-gigante").addEventListener("click", function () {
+    document.querySelector("body").setAttribute("class", "cartola-gigante");
+
+})
+
+document.getElementById("cartola-gigante2").addEventListener("click", function () {
+    document.querySelector("body").setAttribute("class", "cartola-gigant2");
+
+})
